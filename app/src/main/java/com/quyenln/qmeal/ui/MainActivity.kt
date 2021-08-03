fb7af79de3e81c4d166c1ca56d876756f00fcbd2
hideoncalm <hideoncalm@gmail.com>
2021-08-03 20:03:29 +0700

    init

---
+++ app/src/main/java/com/quyenln/qmeal/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.quyenln.qmeal.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.quyenln.qmeal.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
